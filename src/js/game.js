1bdd10923dddfd4abd281963e7fa54fa16521cf7
Soulmaers <certiss@ya.ru>
2022-05-25 08:05:28 +0300

    готово 2-ая задача

---
+++ src/js/game.js
@@ -0,0 +1,18 @@
+import Character from ('./domain');
+export default Game;
+export { GameSavingData, readGameSaving, writeGameSaving }
+
+class Game {
+    start() {
+        console.log('game started');
+    }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
